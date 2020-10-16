995031bc9d3e6484fae92cf5a324958784e3e4e0
yp0975eu <yp0975eu@go.minneapolis.edu>
2020-10-16 12:29:17 -0500

    handle error in response

--- lyrics/musix/musix_usage.py
+++ lyrics/musix/musix_usage.py
@@ -9,13 +9,16 @@ api_key = os.getenv("musix_api_key")
 track = Track(api_key)
 
 results = track.track_search('Believe')
-headers = results.get('message').get('header')
-body = results.get('message').get('body')
+if results.ok and results.status_code == 200:
+    headers = results.get('message').get('header')
+    body = results.get('message').get('body')
 
-print(f"status code: {headers.get('status_code')}")
-print(f"{headers.get('available')} results available\n\n")
+    print(f"status code: {headers.get('status_code')}")
+    print(f"{headers.get('available')} results available\n\n")
 
-track_list = body.get('track_list')
-for t in track_list:
-    track = t.get('track')
-    print(f"{track.get('track_id')} - {track.get('track_name')} - {track.get('album_name')} {track.get('artist_name')}")
+    track_list = body.get('track_list')
+    for t in track_list:
+        track = t.get('track')
+        print(f"{track.get('track_id')} - {track.get('track_name')} - {track.get('album_name')} {track.get('artist_name')}")
+else:
+    print(results.status_code)

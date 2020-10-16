c7dfb2e2a02b5b443248f7eb5271ece0011fc0ce
yp0975eu <yp0975eu@go.minneapolis.edu>
2020-10-16 11:57:58 -0500

    add usage exampe for  musix

---
+++ lyrics/musix/musix_usage.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from Musix import Track
+
+load_dotenv()
+
+api_key = os.getenv("musix_api_key")
+
+track = Track(api_key)
+
+results = track.track_search('Believe')
+headers = results.get('message').get('header')
+body = results.get('message').get('body')
+
+print(f"status code: {headers.get('status_code')}")
+print(f"{headers.get('available')} results available\n\n")
+
+track_list = body.get('track_list')
+for t in track_list:
+    track = t.get('track')
+    print(f"{track.get('track_id')} - {track.get('track_name')} - {track.get('album_name')} {track.get('artist_name')}")

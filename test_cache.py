d1585456a989173f316e93957f6d0318b633260d
yp0975eu <yp0975eu@go.minneapolis.edu>
2020-10-19 18:13:39 -0500

    add test_invalidation to test_cache.py

--- test_cache.py
+++ test_cache.py
@@ -3,15 +3,28 @@ from cache import Cache
 
 
 class TestCache(unittest.TestCase):
-    def test_setting(self):
-        cache = Cache()
-        service_response = {'response': []}
-        key = 'service1'
-        b64d_url = 'abcdef'
-        cache.set(key, b64d_url , service_response, 30)
-        cached_response = cache.get(key, b64d_url)
-        self.assertEqual(service_response, cached_response.get('value'))
 
+
+    def setUp(self):
+        """
+        create a new cache for each test overwriting previous self.cache state
+        """
+        self.service_response = {'response': []}
+        self.key = 'service1'
+        self.b64d_url = 'abcdef'
+        self.cache = Cache()
+        self.cache.set(self.key, self.b64d_url , self.service_response, 30)
+
+
+    def test_setting(self):
+        cached_response = self.cache.get(self.key, self.b64d_url)
+        self.assertEqual(self.service_response, cached_response.get('value'))
+
+    def test_invalidating(self):
+        self.cache.set('somekey2','vavagw',{'f':1}, 30)
+        self.cache.invalidate(self.key)
+        storage = self.cache.storage
+        self.assertEqual(storage, {'somekey2': {'vavagw': {'expiration': 30, 'value': {'f': 1}}}})
 
 
 if __name__ == "__main__":

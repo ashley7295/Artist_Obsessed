51e0442145c4805d70200d0a0faba34d77f2e5a3
ashley7295 <avkellerman@yahoo.com>
2020-10-22 18:54:43 -0500

    added baseic menu and ui functions

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
 #created module to have something in the amster branch
+
+import ui
+#import bookmarks
+
+
+def preform_menu_selection ():
+
+    menu = True
+
+    while menu:
+        user_selection = ui.print_menu()
+        
+        if user_selection == 1:  
+        #enter display bookmarks function
+            pass    
+        elif user_selection == 2: 
+            ui.get_artist_name
+            ui.get_album_name
+            ui.get_song_name
+            
+            search_results()
+
+        elif user_selection == 3: 
+            #bookmarks.search_by_ID
+            pass
+        elif user_selection == 4: 
+            #bookmarks.delete_by_ID
+            pass
+        elif user_selection == 5: 
+            quit_program()
+            menu = False
+
+def quit_program():
+    ui.print_message('Bye and Thank you!')
+
+def search_results():
+    #print = {artist_name} has {followers} on spotify
+    #print = Here is the artwork for {album title}, {album_URL}
+    #print = Here are the lyrics for {song_title}: 
+    #print = {lyrics}
+    pass
+
+
---
+++ ui.py
@@ -0,0 +1,37 @@
+
+
+
+def get_artist_name():
+    artist_name = input('What is the name of the artist you are searching for?: ')
+    return artist_name
+
+def get_album_name():
+    album_name = input('What is the name of the album you are looking for?: ')
+    return album_name
+
+def get_song_name():
+    song_title = input('What is the name of the song you are looking for?: ')
+    return song_title
+
+def save_bookmark():
+    save_bookmark = input('Do you want to save this as a bookmark?: (Y/N) ')
+    
+    if save_bookmark == 'Y' or 'y':
+        return True
+
+def print_message(msg):
+    return print(msg)
+
+def print_menu():
+    print(' 1: Display all Bookmarks')
+    print(' 2: Start Search querie')
+    print(' 3: Search for a bookmark by ID')
+    print(' 4: Delete a Bookmark by ID')
+    print(' 5: Quit')
+
+    user_selection = int(input('Please enter the number of the menu item you would like to select: '))
+
+    if user_selection in range (1, 6):
+        return user_selection
+    else:
+        print('Please enter a valid number between 1 & 5')

365e95c42f5d74f46488e1d1b058b950d0696ff0
ashley7295 <avkellerman@yahoo.com>
2020-10-22 20:24:37 -0500

    added more functions in main for gathering searches and results

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 import ui
 import bookmarks
 
+def main():
+
+    ui.program_intro_message()
+    preform_menu_selection()
 
 def preform_menu_selection ():
 
@@ -13,15 +17,20 @@ def preform_menu_selection ():
         user_selection = ui.print_menu()
         
         if user_selection == 1:  
-        #enter display bookmarks function
-            pass    
-        elif user_selection == 2: 
-            ui.get_artist_name
-            ui.get_album_name
-            ui.get_song_name
             
-            search_results()
+            user_search()
+            print_search_results()
 
+            save = ui.save_bookmark()
+
+            if save == True:
+                save_new_bookmark()
+            elif save == False:
+                print('Your bookmark will not be saved.')
+            
+
+        elif user_selection == 2: 
+            bookmarks.get_all_bookmarks()
         elif user_selection == 3: 
             ID = ui.search_by_id()
             bookmarks.delete_by_id(ID)
@@ -35,13 +44,48 @@ def preform_menu_selection ():
             menu = False
 
 def quit_program():
-    ui.print_message('Bye and Thank you!')
+    ui.print_message('Bye and thank you!')
+
 
 def search_results():
-    #print = {artist_name} has {followers} on spotify
-    #print = Here is the artwork for {album title}, {album_URL}
-    #print = Here are the lyrics for {song_title}: 
-    #print = {lyrics}
+    #TODO set variables for results of our APIs
+
+    #followers = SpotifyAPI
+    #album_artwork = artworkAPI
+    #lyrics = lyricsAPI
+    
+    #return followers, album_artwork, lyrics
     pass
 
-preform_menu_selection()
+#gets variables for the UI of the search queries
+def user_search():
+    artist_name = ui.get_artist_name()
+    album_title = ui.get_album_name()
+    song_title = ui.get_song_name()
+    return artist_name, album_title, song_title
+
+
+def print_search_results():
+    #TODO once we can finish the search_results() function, we can finish this one
+    
+    #artist, album, song = user_search()
+    #followers, album_art, lyrics = search_results()
+    
+    #print = {artist} has {followers} followers on spotify
+    #print = Here is the album artwork for {album}, {album_art}
+    #print = Here are the lyrics for {song}: 
+    #print = {lyrics}
+    pass
+    
+
+def save_new_bookmark():
+    #TODO once search_results() function can be finished this one can be filled in as well
+
+    #artist, album, song = user_search()
+    #followers, album_art, lyrics = search_results()
+
+    #bookmarks.add_new_bookmark(artist, album, song, followers, album_art, lyrics)
+    pass
+
+if __name__ == '__main__':
+    main()
--- ui.py
+++ ui.py
@@ -1,5 +1,10 @@
 
 
+def program_intro_message():
+    print('\n WELCOME TO ~ARTIST OBSESSED~ \n')
+    print('The program will search for information about an artist and provide thier follower count, album artwork and song lyrics.')
+    print('You also can bookmark searches and search from your previously bookmarked searches.')
+    print('please select an option below to begin: \n')
 
 def get_artist_name():
     artist_name = input('What is the name of the artist you are searching for?: ')
@@ -14,10 +19,14 @@ def get_song_name():
     return song_title
 
 def save_bookmark():
-    save_bookmark = input('Do you want to save this as a bookmark?: (Y/N) ')
+    save_bookmark = input('Do you want to save this as a bookmark? (Y/N): ')
     
     if save_bookmark == 'Y' or 'y':
         return True
+    elif save_bookmark == 'N' or 'n': 
+        return False
+    else:
+        print('Please enter a valid Y/N entry.')
 
 def search_by_id():
     ID = int(input('Please enter the ID of the Bookmark you would like to select: '))
@@ -26,9 +35,10 @@ def search_by_id():
 def print_message(msg):
     return print(msg)
 
+
 def print_menu():
-    print(' 1: Display all Bookmarks')
-    print(' 2: Start Search querie')
+    print(' 1: Start Search querie')
+    print(' 2: Display all Bookmarks')
     print(' 3: Search for a bookmark by ID')
     print(' 4: Delete a Bookmark by ID')
     print(' 5: Quit')

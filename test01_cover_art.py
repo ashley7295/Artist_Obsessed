6235ff95339bda0dbd24c843f845bf6190b4be73
“jy8474np” <jy8474np@go.minnstate.edu>
2020-10-29 19:07:31 -0500

    test02_cover_art.py code updated. Remains, non-functional.

--- test01_cover_art.py
+++ test01_cover_art.py
@@ -10,10 +10,10 @@ import cover_art_api
 
 test_url = cover_art_api.url
 test_key = cover_art_api.key
-test_cover_response = cover_art_api.cover_response
+cover_response = cover_art_api.cover_response
 test_cover_response = test_image_download.r
 
-def test_image_download(test_url, test_file_name):
+def test_image_download(self):
     r = requests.get(test_url)
     i = Image.open(BytesIO(r.content))
     i.save(test_file_name)
@@ -21,7 +21,6 @@ def test_image_download(test_url, test_file_name):
     self.assertEqual(cover_art_api.cover_response , test_cover_response)
 
 if __name__ == "__main__":
-
-    t_url = cover_art_api.image_url
-    test_file = "test_image_download.png"
-    test_image_download(t_url, test_file)
+    t_url = test_cover_response.image_url
+    file = "test_image_download.png"
+    test_image_download(t_url, file)

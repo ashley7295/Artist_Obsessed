409945262f81eb64798158ea1d3ee7259aa43982
“jy8474np” <jy8474np@go.minnstate.edu>
2020-11-01 19:53:58 -0600

    cover_art test complete and functional

--- cover_art_api.py
+++ cover_art_api.py
@@ -24,11 +24,13 @@ def get_album_art(artist, album): # Searches for album artwork from API query re
         return "Could not find artwork" # Display error reason to user
 
 def get_image_by_size(images, size): # Searches for album art by size specifications
-    for i in images: # Looks within API’s “images”
-        if i.get("size") == size: # Looks for size
-            image_url = i.get("#text") # Locates size-specific image
+    image_list = images["album"]["image"]
+    for i in image_list: # Looks within API’s “images”
+        if i["size"] == size: # Looks for size
+            image_url = i["#text"] # Locates size-specific image
             return image_url # Returns size-specific image url
 
+# Function not required as part of current program. May be a useful function in future.
 def image_download(url, file_name): # Save and displays image for user
     r = requests.get(url) # Open url
     i = Image.open(BytesIO(r.content)) # Open desired cover art image image
--- test_cover_art_api.py
+++ test_cover_art_api.py
@@ -17,27 +17,10 @@ class TestCover(unittest.TestCase):
         test_query_results = requests.get(test_url , params=test_query).json()
         expected_query_results = requests.get(cover_art_api.url, params=test_query).json() # Query response with requested data
         self.assertEqual(test_query_results , expected_query_results) # Confirm test results equal expected results
+ 
+    # image size is tested within cover_art_api.get_album_art
 
-    def test_get_image_by_size(self):
-        r = requests.get(cover_art_api.url) # Obtain url from cover_art_api.py
-        test_images = r.get("album").get("image") # Looks within API’s “images”
-        for i in test_images:
-            # size variable below has squigglies as "undefined variable"
-            if i.get("size") == size: # Looks for specific image size
-                test_image_url = i.get("#text") # Locates size-specific image
-                return test_image_url # Returns size-specific image
-        expected_url = cover_art_api.get_image_by_size("images" , "size") # Expected cover_art_api.py results
-        self.assertEqual(test_image_url , expected_url) # Confirm test results equal expected results
-
-    def test_image_download(self):
-        testValue = True # Set test value to True for future data comparison
-        r = requests.get(cover_art_api.url) # Obtain url from cover_art_api.py
-        i = Image.open(BytesIO(r.content)) # Open desired cover art image image
-        test_file_name = "test_image_download.png" # Assign file name
-        save = i.save(test_file_name) # Save cover art image as a assigned file name
-        show = i.show(test_file_name) # Display album cover art to user
-        self.assertTrue(testValue , show) # Confirm test results equal expected results
-        self.assertTrue(testValue , save) # Confirm test results equal expected results
+    # cover_art_api.image_download was created as an "bonus" function
 
 if __name__ == "__main__":
     unittest.main()

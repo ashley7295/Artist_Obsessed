9c1c9efce62edcd367941c343c12df9b3e1ec9c7
“jy8474np” <jy8474np@go.minnstate.edu>
2020-11-01 18:28:50 -0600

    Cover testing w test 1 working and minimal errors. Will review w Ashley.

--- test_cover_art_api.py
+++ test_cover_art_api.py
@@ -10,11 +10,13 @@ class TestCover(unittest.TestCase):
 
     def test_get_album_art(self):
         test_key = cover_art_api.key # Obtain cover art key from cover_art_api.py
+        test_url = cover_art_api.url
         test_album = "Lemonade" # Test album name
         test_artist = "Beyonce" # Test artist name
         test_query = {"method" : "album.getinfo", "api_key" : test_key, "artist" : test_artist, "album" : test_album, "format" : "json"} # Test query with parameters 
-        test_query_results = cover_art_api.get_album_art(test_album, test_artist) # Obtain test query results
-        self.assertEqual(test_query , test_query_results) # Confirm test results equal expected results
+        test_query_results = requests.get(test_url , params=test_query).json()
+        expected_query_results = requests.get(cover_art_api.url, params=test_query).json() # Query response with requested data
+        self.assertEqual(test_query_results , expected_query_results) # Confirm test results equal expected results
 
     def test_get_image_by_size(self):
         r = requests.get(cover_art_api.url) # Obtain url from cover_art_api.py
